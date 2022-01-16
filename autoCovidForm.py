81b412e90fbeaacdf15d12d13778fe05aa3379ea
Matthew Zelenka <MZelenka04@gmail.com>
2022-01-15 20:52:39 -0500

    Changed selectors

--- autoCovidForm.py
+++ autoCovidForm.py
@@ -23,16 +23,6 @@ valURL = re.compile( # regex to see if valid url
 configFile = "user.json"
 
 class autoForm(webScraper.baseChromeWebScraper):
-    # def __init__(self, url, webDriverPath="./chromedriver", browser=None, browserHide = False):
-    #     #url of the page we want to run
-    #     self.url = url
-    #     self.webDriverPath = webDriverPath
-    #     self.browser = browser
-    #     self.browserHide = browserHide
-
-    # def waitUrlChange(self, currentURL): # function to wait for next page to load before continuing 
-    #     WebDriverWait(self.driver, 10).until(lambda driver: driver.current_url != currentURL)
-
     def autoLogin(self):
         while True:
             currentUrl = self.driver.current_url
@@ -61,8 +51,8 @@ class autoForm(webScraper.baseChromeWebScraper):
             
             elif currentUrl.find("https://accounts.google.com/speedbump/") != -1: # next step in google log in for password
                 print("Speedbumping...")
-                WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#view_container > div > div > div.pwWryf.bxPAYd > div > div.zQJV3 > div > div.qhFLie > div > div > button")))
-                self.driver.find_element(By.CSS_SELECTOR, "#view_container > div > div > div.pwWryf.bxPAYd > div > div.zQJV3 > div > div.qhFLie > div > div > button").click()
+                WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "VfPpkd-LgbsSe.VfPpkd-LgbsSe-OWXEXe-k8QpJ.VfPpkd-LgbsSe-OWXEXe-dgl2Hf.nCP5yc.AjY5Oe.DuMIQc.qIypjc.TrZEUc.lw1w4b")))
+                self.driver.find_element(By.CLASS_NAME, "VfPpkd-LgbsSe.VfPpkd-LgbsSe-OWXEXe-k8QpJ.VfPpkd-LgbsSe-OWXEXe-dgl2Hf.nCP5yc.AjY5Oe.DuMIQc.qIypjc.TrZEUc.lw1w4b").click()
                 self.waitUrlChange(currentUrl)
             
             elif currentUrl.find("https://google.yrdsb.ca/LoginFormIdentityProvider/Login.aspx?") != -1: # YRDSB has stupid special login page for google accounts so it goes through that
@@ -90,19 +80,50 @@ class autoForm(webScraper.baseChromeWebScraper):
             currentUrl = self.driver.current_url
             if currentUrl.find("https://docs.google.com/forms/d/e/1FAIpQLSedNWLgRdQKVfNqT4gwYrq0PEJqj2vnOL5GHqfopjwnakC-0g/viewform") != -1: # fills out form 
                 print("Filling out form...")
+                WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "appsMaterialWizButtonPaperbuttonLabel.quantumWizButtonPaperbuttonLabel.exportLabel")))
+                clearForm = self.driver.find_element(By.CLASS_NAME, "freebirdFormviewerViewNavigationClearButton").find_element(By.CLASS_NAME, "appsMaterialWizButtonPaperbuttonLabel.quantumWizButtonPaperbuttonLabel.exportLabel")
+                clearForm.click()
+                WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "appsMaterialWizDialogPaperdialogEl.freebirdFormviewerViewNavigationClearDialog.appsMaterialWizDialogPaperdialogTransitionZoom.appsMaterialWizDialogEl.isOpen")))
+                clearFormButtons = self.driver.find_element(By.CLASS_NAME, "appsMaterialWizDialogPaperdialogEl.freebirdFormviewerViewNavigationClearDialog.appsMaterialWizDialogPaperdialogTransitionZoom.appsMaterialWizDialogEl.isOpen").find_elements(By.CLASS_NAME, "appsMaterialWizButtonPaperbuttonLabel.quantumWizButtonPaperbuttonLabel.exportLabel")
+                for button in clearFormButtons:
+                    if button.text == "Clear form":
+                        button.click()
+                # WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#mG61Hd > div.freebirdFormviewerViewFormCard.exportFormCard > div > div.freebirdFormviewerViewNavigationNavControls > div.freebirdFormviewerViewNavigationButtonsAndProgress.hasClearButton > div.freebirdFormviewerViewNavigationClearButton > div")))
+                # clearForm = self.driver.find_element(By.CSS_SELECTOR, "#mG61Hd > div.freebirdFormviewerViewFormCard.exportFormCard > div > div.freebirdFormviewerViewNavigationNavControls > div.freebirdFormviewerViewNavigationButtonsAndProgress.hasClearButton > div.freebirdFormviewerViewNavigationClearButton > div")
+                # clearForm.click()
+                # WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "body > div.appsMaterialWizDialogBackground.isOpen > div > div.appsMaterialWizDialogPaperdialogEl.freebirdFormviewerViewNavigationClearDialog.appsMaterialWizDialogPaperdialogTransitionZoom.appsMaterialWizDialogEl.isOpen > div.appsMaterialWizDialogPaperdialogBottomButtons.exportButtons > div:nth-child(2)")))
+                # clearForm = self.driver.find_element(By.CSS_SELECTOR, "body > div.appsMaterialWizDialogBackground.isOpen > div > div.appsMaterialWizDialogPaperdialogEl.freebirdFormviewerViewNavigationClearDialog.appsMaterialWizDialogPaperdialogTransitionZoom.appsMaterialWizDialogEl.isOpen > div.appsMaterialWizDialogPaperdialogBottomButtons.exportButtons > div:nth-child(2)")
+                # clearForm.click()
+                time.sleep(1)
+                WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "freebirdFormviewerComponentsQuestionBaseRoot")))
+                questions = self.driver.find_elements(By.CLASS_NAME, "freebirdFormviewerComponentsQuestionBaseRoot")
+                print(len(questions))
                 with open(configFile, "r") as read_file: # puts email in to google login from user.json
                     data = json.load(read_file)
-                    WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "quantumWizTextinputPaperinputInput")))
-                    textBoxes = self.driver.find_elements(By.CLASS_NAME, "quantumWizTextinputPaperinputInput")
-                    textBoxes[0].send_keys(data["user"]["firstName"])
-                    textBoxes[1].send_keys(data["user"]["lastName"])
-                    WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "appsMaterialWizToggleRadiogroupOffRadio")))
-                    radioButton = self.driver.find_elements(By.CLASS_NAME, "appsMaterialWizToggleRadiogroupOffRadio")
-                    radioButton[0].click()
-                    time.sleep(5)
-                    WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "appsMaterialWizButtonPaperbuttonContent")))
-                    submit = self.driver.find_element(By.CLASS_NAME, "appsMaterialWizButtonPaperbuttonContent")
-                    submit.click()
+                    for question in questions:
+                        WebDriverWait(question, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "freebirdFormviewerComponentsQuestionBaseHeader")))
+                        print("\""+question.find_element(By.CLASS_NAME, "freebirdFormviewerComponentsQuestionBaseHeader").text+"\"")                        
+                        match (str(question.find_element(By.CLASS_NAME, "freebirdFormviewerComponentsQuestionBaseHeader").text)):
+                            case "Student First Name *":
+                                WebDriverWait(question, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "quantumWizTextinputPaperinputInput")))
+                                textbox = question.find_element(By.CLASS_NAME, "quantumWizTextinputPaperinputInput")
+                                textbox.send_keys(data["user"]["firstName"])
+                            case "Student Last Name *":
+                                WebDriverWait(question, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "quantumWizTextinputPaperinputInput")))
+                                textbox = question.find_element(By.CLASS_NAME, "quantumWizTextinputPaperinputInput")
+                                textbox.send_keys(data["user"]["lastName"])
+                            case "Have you completed the self-screening test? *\nCOVID 19 School and Child Care Screening Tool is available at https://covid-19.ontario.ca/school-screening/":
+                                choices = question.find_elements(By.CLASS_NAME, "freebirdFormviewerComponentsQuestionRadioChoice.freebirdFormviewerComponentsQuestionRadioOptionContainer")
+                                for choice in choices:
+                                    if choice.text == "Yes":
+                                        choice.click()
+                            case _:
+                                print("Question not found")
+                time.sleep(5)
+                WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "freebirdFormviewerViewNavigationLeftButtons")))
+                submit = self.driver.find_element(By.CLASS_NAME, "freebirdFormviewerViewNavigationLeftButtons").find_element(By.CLASS_NAME, "appsMaterialWizButtonPaperbuttonContent")
+                print(submit.text)
+                # submit.click()
                 self.waitUrlChange(currentUrl)
                 break
             elif currentUrl.find("https://docs.google.com/forms/d/e/1FAIpQLSedNWLgRdQKVfNqT4gwYrq0PEJqj2vnOL5GHqfopjwnakC-0g/closedform") != -1: 
@@ -115,13 +136,10 @@ class autoForm(webScraper.baseChromeWebScraper):
                 print("Form is closed")
                 break
 
-
     def run(self):
         self.setup()
         self.autoLogin()
         self.fillForm()
-
-
 
 # Program starts running
 if __name__ == '__main__':
