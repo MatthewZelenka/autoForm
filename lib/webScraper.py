b84495d3053b4ec647e3d244285aa7e9b101b788
Matthew Zelenka <MZelenka04@gmail.com>
2022-02-09 18:51:31 -0500

    Added import

--- lib/webScraper.py
+++ lib/webScraper.py
@@ -3,6 +3,7 @@ import selenium, importlib
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
+from selenium.common import exceptions
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support.ui import WebDriverWait
