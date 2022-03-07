754f53da2fc00d568e5bbd4bb91ad80191465c80
Matthew Zelenka <mzelenka04@gmail.com>
2022-03-06 21:17:21 -0500

    Added return values

--- lib/google/autoLogin.py
+++ lib/google/autoLogin.py
@@ -4,53 +4,54 @@ from dataclasses import dataclass
 @dataclass
 class googleRedirect:
     url = "https://accounts.google.com/ServiceLogin?service"
-    def doPage(self, profile):
-        print("Google login redirecting...")
+    def doPage(self, profile) -> str:
+        return "Google login redirected"
 
 @dataclass
 class googleEmail:
     url = "https://accounts.google.com/signin/v2/identifier?service"
-    def doPage(self, profile):
-        print("Logging in to google...")
+    def doPage(self, profile) -> str:
         WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".whsOnd.zHQkBf")))
         login = self.driver.find_element(By.CSS_SELECTOR, ".whsOnd.zHQkBf")
         login.send_keys(profile["user"]["email"])
         WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "VfPpkd-dgl2Hf-ppHlrf-sM5MNb")))
         self.driver.find_element(By.CLASS_NAME, "VfPpkd-dgl2Hf-ppHlrf-sM5MNb").click()
+        return "Logged in to google"
 
 @dataclass
 class googlePassword:
     url = "https://accounts.google.com/signin/v2/challenge/pwd?"
-    def doPage(self, profile):
-        print("Putting in password...")
+    def doPage(self, profile) -> str:
         WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".whsOnd.zHQkBf")))
         login = self.driver.find_element(By.CSS_SELECTOR, ".whsOnd.zHQkBf")
         login.send_keys(profile["user"]["password"]) 
         WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "VfPpkd-dgl2Hf-ppHlrf-sM5MNb")))
         self.driver.find_element(By.CLASS_NAME, "VfPpkd-dgl2Hf-ppHlrf-sM5MNb").click()
+        return "Submited password"
 
 @dataclass
 class googleContinuing:
     url = "https://accounts.google.com/signin/continue?"
-    def doPage(self, profile):
-        print("Google login continuing...")
+    def doPage(self, profile) -> str:
+        return "Google login continued"
 
 @dataclass
 class googleSpeedbump:
     url = "https://accounts.google.com/speedbump/"
-    def doPage(self, profile):
-        print("Speedbumping...")
+    def doPage(self, profile) -> str:
         WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "VfPpkd-LgbsSe.VfPpkd-LgbsSe-OWXEXe-k8QpJ.VfPpkd-LgbsSe-OWXEXe-dgl2Hf.nCP5yc.AjY5Oe.DuMIQc.qIypjc.TrZEUc.lw1w4b")))
         self.driver.find_element(By.CLASS_NAME, "VfPpkd-LgbsSe.VfPpkd-LgbsSe-OWXEXe-k8QpJ.VfPpkd-LgbsSe-OWXEXe-dgl2Hf.nCP5yc.AjY5Oe.DuMIQc.qIypjc.TrZEUc.lw1w4b").click()
+        return "Speedbumped"
 
-def login(self, profile, pageProcedures:list=[]):
+def login(self, profile, pageProcedures:list=[], printOut=False):
     pageProcedure = [googleRedirect, googleEmail, googlePassword, googleContinuing, googleSpeedbump]
     pageProcedure.extend(pageProcedures)
     while True:
         currentUrl = self.driver.current_url
         for procedure in pageProcedure:
             if currentUrl.find(procedure.url) != -1:
-                procedure.doPage(self, profile)
+                process = procedure.doPage(self, profile)
+                if printOut: print(process)
                 break
             elif procedure == pageProcedure[-1]:
                 return
--- plugins/yrdsbCovidForm.py
+++ plugins/yrdsbCovidForm.py
@@ -6,22 +6,21 @@ from lib.webScraper import *
 
 plugName = pathlib.Path(__file__).stem
 
-# form = "https://docs.google.com/forms/d/e/1FAIpQLSe7KN_LlhllKPWnBJANeZf3cNFbBijRcCtj0Jf3ARQ0mUqZ7w/viewform"
+form = "https://docs.google.com/forms/d/e/1FAIpQLSe7KN_LlhllKPWnBJANeZf3cNFbBijRcCtj0Jf3ARQ0mUqZ7w/viewform"
 
-form = "https://docs.google.com/forms/d/e/1FAIpQLSedNWLgRdQKVfNqT4gwYrq0PEJqj2vnOL5GHqfopjwnakC-0g/viewform"
+# form = "https://docs.google.com/forms/d/e/1FAIpQLSedNWLgRdQKVfNqT4gwYrq0PEJqj2vnOL5GHqfopjwnakC-0g/viewform"
 
 # page procedures when in yrdsb login page
 @dataclass
 class yrdsbRedirect:
     url = "https://google.yrdsb.ca/EasyConnect/"
-    def doPage(self, profile):
-        print("YRDSB redirection...")
+    def doPage(self, profile) -> str:
+        return "YRDSB redirection"
 
 @dataclass
 class yrdsbLogin:
     url = "https://google.yrdsb.ca/LoginFormIdentityProvider/Login.aspx?"
-    def doPage(self, profile):
-        print("Logging in to YRDSB...")
+    def doPage(self, profile) -> str:
         WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, "UserName")))
         login = self.driver.find_element(By.ID, "UserName")
         login.send_keys(profile["user"]["userName"])
@@ -30,6 +29,7 @@ class yrdsbLogin:
         login.send_keys(profile["user"]["password"])
         WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.NAME, "LoginButton")))
         self.driver.find_element(By.NAME, "LoginButton").click()
+        return "Logging in to YRDSB"
 
 def waitToComplete(self, waitTime: int = 10): # function to wait for next page to load before continuing 
     try:
@@ -40,7 +40,7 @@ def waitToComplete(self, waitTime: int = 10): # function to wait for next page t
 
 def fillForm(self, profile):
     # does google login and attaches yrdsb login code
-    google.autoLogin.login(self=self, profile=profile, pageProcedures=[yrdsbRedirect, yrdsbLogin])
+    google.autoLogin.login(self=self, profile=profile, pageProcedures=[yrdsbRedirect, yrdsbLogin], printOut=True)
 
     # fills google form
     if google.formFiller.getFormState(self) == "uncomplete": # fills out form 
@@ -53,7 +53,7 @@ def fillForm(self, profile):
         covidScreeningV2 = google.formFiller.multipleChoice(question="Have you completed the self-screening test? *\nCOVID 19 School and Child Care Screening Tool is available at https://covid-19.ontario.ca/school-screening/ *Important Update: The Ministry Of Education and the Chief Medical Officer of Health https://www2.yrdsb.ca/sites/default/files/2022-01/EDU-JointParentsLetterJanuary182022-EN.pdf have provided additional information that may affect your screening results, including:", answer=["Yes"])
 
         google.formFiller.fillForm(self, firstName, lastName, covidScreeningV1, covidScreeningV2)
-        time.sleep(5)
+        time.sleep(2)
         google.formFiller.submit(self)
     waitToComplete(self)
     print("Form is {}".format(google.formFiller.getFormState(self)))
